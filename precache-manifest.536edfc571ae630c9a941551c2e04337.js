247df2a912b244639b79d6e1d33da15145b66a2a
ico4e <70281513+ico4e@users.noreply.github.com>
2020-09-19 08:18:44 -0400

    Initial dist subtree commit

---
+++ precache-manifest.536edfc571ae630c9a941551c2e04337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55aefe967157ef8bffe65af57eb04187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25d6ec1b846e83a9743e",
+    "url": "/static/css/main.fa88ddec.chunk.css"
+  },
+  {
+    "revision": "cf59b50f05ee1cb953e6",
+    "url": "/static/js/2.876369cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.876369cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d6ec1b846e83a9743e",
+    "url": "/static/js/main.0fffd989.chunk.js"
+  },
+  {
+    "revision": "bfce0520712d22704737",
+    "url": "/static/js/runtime-main.9901dcf2.js"
+  }
+]);
