2ea09adb992a48490ae5fc9fef99841714d1e8b7
ico4e <70281513+ico4e@users.noreply.github.com>
2020-09-20 18:02:36 -0400

    new build with Search added

---
+++ precache-manifest.ef32a7b57d06d89bda96e8cf2e40ad2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2420cda485db519e2ec180cb71fa5ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d31cb2f59faff22bd3a7",
+    "url": "/static/css/main.683e2481.chunk.css"
+  },
+  {
+    "revision": "da2d2de67aa005c8c2a7",
+    "url": "/static/js/2.c326667b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c326667b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31cb2f59faff22bd3a7",
+    "url": "/static/js/main.a1fe08cd.chunk.js"
+  },
+  {
+    "revision": "bfce0520712d22704737",
+    "url": "/static/js/runtime-main.9901dcf2.js"
+  }
+]);
